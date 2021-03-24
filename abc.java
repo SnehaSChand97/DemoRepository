6a37b8d5b1f93458b83841c45ceecd44637eaf62
SnehaSChand97 <sneha.chand@ibm.com>
2021-03-24 17:12:39 +0530

    altered abc.java and constructed xyz.java

--- abc.java
+++ abc.java
@@ -3,5 +3,10 @@ public class abc
   public static void main(String args[])
   {
     System.out.println("hello");
+     for(int i=1;i<=10;i++)
+    {
+      System.out.println("hi"); 
+     }
+   
   }
 }
